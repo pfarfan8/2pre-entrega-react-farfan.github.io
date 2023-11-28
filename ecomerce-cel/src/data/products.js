9d41c9ee011f38bf5d895cabe0927fb5cf856ee1
pfarfan8 <114534818+pfarfan8@users.noreply.github.com>
2023-11-28 19:54:24 -0300

    Update products.js

--- ecomerce-cel/src/data/products.js
+++ ecomerce-cel/src/data/products.js
@@ -1,26 +1,51 @@
 export const products = [
+
     {
+
         id: 1,
+
         titulo: "celu",
+
         categoria: "iphone",
+
         descripcion: "celu nuevo",
+
         precio: 100,
-        img: "../assets/iphone.jpg"
+
+        img: "../src/assets/iphone.jpg"
+
     },
+
     {
+
         id: 2,
+
         titulo: "celu2",
+
         categoria: "samsung",
+
         descripcion: "celu nuevo",
+
         precio: 100,
-        img: "../assets/samsung.jpg"
+
+        img: "../src/assets/samsung.jpg"
+
     },
+
     {
+
         id: 3,
+
         titulo: "celu3",
+
         categoria: "otros",
+
         descripcion: "celu nuevo",
+
         precio: 100,
-        img: "../assets/xiaomi.jpeg"
+
+        img: "../src/assets/xiaomi.jpg"
+
     },
+
 ];
